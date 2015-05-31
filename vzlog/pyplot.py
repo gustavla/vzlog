8c1477b8bec2058e5305a259a6767bbc9e6545d6
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-05-31 11:17:41 -0500

    Removed margins from matplotlib figures.

--- vzlog/pyplot.py
+++ vzlog/pyplot.py
@@ -3,5 +3,7 @@ from __future__ import division, print_function, absolute_import
 import matplotlib
 matplotlib.use('Agg')
 matplotlib.rc('font', size=8)
+matplotlib.rc('savefig', bbox='tight')
+
 from matplotlib.pyplot import *
 style.use('bmh')
