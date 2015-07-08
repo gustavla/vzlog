01805a88d2467f326e5d2d7a667bcfc9c033a105
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-07-07 19:23:23 -0500

    Starting version 0.1.4

--- vzlog/__init__.py
+++ vzlog/__init__.py
@@ -2,8 +2,8 @@ from __future__ import division, print_function, absolute_import
 
 from .vzlog import VzLog
 
-VERSION = (0, 1, 3)
-ISRELEASE = True
+VERSION = (0, 1, 4)
+ISRELEASE = False
 __version__ = '{0}.{1}.{2}'.format(*VERSION)
 if not ISRELEASE:
     __version__ += '.git'
