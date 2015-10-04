48046f963ce2a668f29af73724db31432edbe796
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-10-04 12:44:13 -0500

    Added short-cuts to ImageGrid/ColorImageGrid.

--- vzlog/__init__.py
+++ vzlog/__init__.py
@@ -3,6 +3,8 @@ from __future__ import division, print_function, absolute_import
 from vzlog.vzlog import VzLog
 from vzlog import image
 
+from vzlog.image import ImageGrid, ColorImageGrid
+
 VERSION = (0, 1, 8)
 ISRELEASE = False
 __version__ = '{0}.{1}.{2}'.format(*VERSION)
